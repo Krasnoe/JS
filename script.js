96eb1490b17aa14f2853b44c1d07e4a7cded2e81
Krasnoe <psp.tema@mail.ru>
2021-05-31 09:22:00 +0300

    feat(scripts.js): add lesson_02

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 'use strict';
 
-let money,
-  income,
-  addExpenses,
-  deposit,
-  mission,
-  period;
+// Доход за месяц
+let money = 50000;
+// Дополнительный доход
+let income = 20000;
+// Дополнительные расходы
+let addExpenses= 'еда, кальян, виски';
+// Любое булево значение
+let deposit = true;
+// Сумма, которую хочу накопить
+let mission = 350000;
+// От 1 до 12 месяцев
+let period = 12;
 
-alert('Hello, world');
-console.log('Message in console');
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log([addExpenses.toLowerCase()]);
+
+// Дневной бюджет
+let budgetDay = money / 30;
+console.log(budgetDay);
