91cbc15bb50db719ae9fc1ab8966146933c8a527
Krasnoe <psp.tema@mail.ru>
2021-06-08 21:31:01 +0300

    fix(script.js): add tabs to all "if"

--- script.js
+++ script.js
@@ -5,34 +5,31 @@ const bot = function(){
   function game() {
     const userNumber = prompt('Угадай число от 1 до 100');
     
-  if (userNumber > botNumber) {
-    alert('Загаданное число меньше');
-    console.log(userNumber);
-    bot();
+    if (userNumber > botNumber) {
+      alert('Загаданное число меньше');
+      console.log(userNumber);
+      bot();
+    } 
 
-  } 
+    if (userNumber < botNumber && userNumber > 0){
+      alert('Загаданное число больше');
+      console.log(userNumber);
+      bot();
+    } 
+    if (userNumber == botNumber){
+      alert('Поздравляю, Вы угадали!!!');
+    } 
+    if (userNumber === null){
+      console.log(userNumber);
+      alert('Игра окончена');
+      return;
+    }
 
-  if (userNumber < botNumber && userNumber > 0){
-    alert('Загаданное число больше');
-    console.log(userNumber);
-    bot();
-
-  } 
-  if (userNumber == botNumber){
-    alert('Поздравляю, Вы угадали!!!');
-  } 
-
-  if (userNumber === null){
-    console.log(userNumber);
-    alert('Игра окончена');
-    return;
-  }
-
-  if (isNaN(userNumber) || userNumber == 0 || userNumber.trim() === ''){
-    console.log(userNumber);
-    alert('Введи число!');
-    bot();
-  }
+    if (isNaN(userNumber) || userNumber == 0 || userNumber.trim() === ''){
+      console.log(userNumber);
+      alert('Введи число!');
+      bot();
+    }
   }
   game();
 };
