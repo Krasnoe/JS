6faf9389364ce519031c2c0799ff5a1674d7a878
Krasnoe <psp.tema@mail.ru>
2021-07-05 17:44:35 +0300

    fix(script.js): added function zero, del node_modules

--- script/script.js
+++ script/script.js
@@ -7,42 +7,37 @@ window.addEventListener('DOMContentLoaded', function() {
         timerMinutes = document.querySelector('#timer-minutes'),
         timerSeconds = document.querySelector('#timer-seconds');
 
-  function getTimeRemaining(){
-    let dateStop = new Date(deadline).getTime(),
-      dateNow = new Date().getTime(),
-      timeRemaining = (dateStop - dateNow) / 1000,
-      seconds = Math.floor(timeRemaining % 60),
-      minutes = Math.floor((timeRemaining / 60) % 60),
-      hours = Math.floor(timeRemaining / 60 / 60);
-      return {timeRemaining, hours, minutes, seconds};
-  }
-  function updateClock(){
-    let timer = getTimeRemaining();
-    if(timer.hours < 10){
-      timerHours.textContent = '0' + timer.hours;
-    } else {
-      timerHours.textContent = timer.hours;
+    function getTimeRemaining(){
+      let dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor(timeRemaining / 60 / 60);
+        return {timeRemaining, hours, minutes, seconds};
     }
-    if(timer.minutes < 10){
-      timerMinutes.textContent = '0' + timer.minutes;
-    } else {
-      timerMinutes.textContent = timer.minutes;
+    function zero(n, obj) {
+      if(n < 10){
+        obj.textContent = '0' + n;
+      } else {
+        obj.textContent = n;
+      }
     }
-    if(timer.seconds < 10){
-      timerSeconds.textContent = '0' + timer.seconds;
-    } else {
-      timerSeconds.textContent = timer.seconds;
+    function updateClock(){
+      let timer = getTimeRemaining();
+      zero(timer.hours, timerHours);
+      zero(timer.minutes, timerMinutes);
+      zero(timer.seconds, timerSeconds);
     }
-  }
-  let idInterval = setInterval(updateClock, 1000),
-      timer = getTimeRemaining().timeRemaining;
+    let idInterval = setInterval(updateClock, 1000),
+        timer = getTimeRemaining().timeRemaining;
 
-  if (timer === 0 || timer < 0) {
-      timerHours.textContent = '00';
-      timerMinutes.textContent = '00';
-      timerSeconds.textContent = '00';
-      clearInterval(idInterval);
-  }
+    if (timer === 0 || timer < 0) {
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+        clearInterval(idInterval);
+    }
 
   }
   countTimer('31 december 2022');
