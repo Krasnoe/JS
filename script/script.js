80b1c433f1a30c94ac529fde2dc47f7339da7bb3
Krasnoe <psp.tema@mail.ru>
2021-07-07 14:04:48 +0300

    feat(script.js): added f togglePopUp

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
   'use strict';
 
-  // timer
+  // timer **************************************************************
   function countTimer(deadline) {
     let timerHours = document.querySelector('#timer-hours'),
         timerMinutes = document.querySelector('#timer-minutes'),
@@ -46,6 +46,63 @@ window.addEventListener('DOMContentLoaded', function() {
 
   }
   countTimer('31 december 2022');
-  // countTimer('05 jule 2021');
+
+  // menu********************************************************************
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+          menu = document.querySelector('menu'),
+          closeBtn = document.querySelector('.close-btn'),
+          menuItems = menu.querySelectorAll('ul>li');
+    
+    const handlerMenu = () => {
+      menu.classList.toggle('active-menu');
+    };
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+    
+  };
+  toggleMenu();
+
+  // popup**************************************************************************
+  const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+          popupBtn = document.querySelectorAll('.popup-btn'),
+          popupClose = document.querySelector('.popup-close'),
+          popupContent = document.querySelector('.popup-content');
+
+    let count = 0;
+    let flyInterval;
+    let flyAnimate = () => {
+      count++;
+      if(count < 361){
+        popupContent.style.transform = `rotate(${count}deg)`;
+      } else {
+        cancelAnimationFrame(flyInterval);
+        count = 0;
+        return;
+      }
+      flyInterval = requestAnimationFrame(flyAnimate);
+    };
+    
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        if(document.documentElement.clientWidth < 768){
+          popup.style.display = 'block';
+        } else {
+          popup.style.display = 'block';
+          flyInterval = requestAnimationFrame(flyAnimate);
+        }
+      });
+    });
+
+    popupClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+    });
+    
+  };
+  
+  togglePopUp();
+
 
 });
