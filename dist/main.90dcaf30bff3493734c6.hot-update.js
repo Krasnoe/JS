bd4abe41a6ac32eace4e8213351c5a52f485fdf8
Krasnoe <psp.tema@mail.ru>
2021-08-13 21:34:02 +0300

    feat(bundle.js): added SliderCarousel

---
+++ dist/main.90dcaf30bff3493734c6.hot-update.js
@@ -0,0 +1,30 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_3DGlo"]("main",{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_countTimer__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/countTimer */ \"./src/modules/countTimer.js\");\n/* harmony import */ var _modules_toggleMenu__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/toggleMenu */ \"./src/modules/toggleMenu.js\");\n/* harmony import */ var _modules_togglePopUp__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/togglePopUp */ \"./src/modules/togglePopUp.js\");\n/* harmony import */ var _modules_tabs__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/tabs */ \"./src/modules/tabs.js\");\n/* harmony import */ var _modules_slider__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules/slider */ \"./src/modules/slider.js\");\n/* harmony import */ var _modules_changeFoto__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules/changeFoto */ \"./src/modules/changeFoto.js\");\n/* harmony import */ var _modules_check__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./modules/check */ \"./src/modules/check.js\");\n/* harmony import */ var _modules_calc__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./modules/calc */ \"./src/modules/calc.js\");\n/* harmony import */ var _modules_sendForm__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./modules/sendForm */ \"./src/modules/sendForm.js\");\n/* harmony import */ var _modules_maskPhone__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./modules/maskPhone */ \"./src/modules/maskPhone.js\");\n\n\n\n\n\n\n\n\n\n\n\n\n // timer\n\n(0,_modules_countTimer__WEBPACK_IMPORTED_MODULE_0__.default)('16 august 2021'); // menu\n\n(0,_modules_toggleMenu__WEBPACK_IMPORTED_MODULE_1__.default)(); // popup\n\n(0,_modules_togglePopUp__WEBPACK_IMPORTED_MODULE_2__.default)(); // табы\n\n(0,_modules_tabs__WEBPACK_IMPORTED_MODULE_3__.default)(); // слайдер\n\n(0,_modules_slider__WEBPACK_IMPORTED_MODULE_4__.default)(1500, true); // смена фотографий\n\n(0,_modules_changeFoto__WEBPACK_IMPORTED_MODULE_5__.default)(); // Дата Атрибуты, Регулярные Выражения\n\n(0,_modules_check__WEBPACK_IMPORTED_MODULE_6__.default)(); // calc\n\n(0,_modules_calc__WEBPACK_IMPORTED_MODULE_7__.default)(); // send-ajax-form\n\n(0,_modules_sendForm__WEBPACK_IMPORTED_MODULE_8__.default)(); // маска для поля телефона\n\n(0,_modules_maskPhone__WEBPACK_IMPORTED_MODULE_9__.default)('#form1-phone, #form2-phone, #form3-phone'); // слайдер Карусель\n\nvar carousel = new _modules_maskPhone__WEBPACK_IMPORTED_MODULE_9__.default({\n  main: '.companies-wrapper',\n  wrap: '.companies-hor',\n  slidesToShow: 4,\n  infinity: true,\n  responsive: [{\n    breakpoint: 1024,\n    slidesToShow: 3\n  }, {\n    breakpoint: 768,\n    slidesToShow: 2\n  }, {\n    breakpoint: 576,\n    slidesToShow: 1\n  }]\n});\ncarousel.init();\n\n//# sourceURL=webpack://3DGlo/./src/index.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("06d1567f00faee32c702")
+/******/ })();
+/******/ 
+/******/ }
+);
--- src/index.js
+++ src/index.js
@@ -10,9 +10,10 @@ import check from './modules/check';
 import calc from './modules/calc';
 import sendForm from './modules/sendForm';
 import maskPhone from './modules/maskPhone';
+import SliderCarousel from './modules/SliderCarousel';
 
   // timer
-  countTimer('12 august 2021');
+  countTimer('16 august 2021');
 
   // menu
   toggleMenu();
@@ -40,3 +41,25 @@ import maskPhone from './modules/maskPhone';
   
   // маска для поля телефона
   maskPhone('#form1-phone, #form2-phone, #form3-phone');
+
+  // слайдер Карусель
+  const carousel = new SliderCarousel({
+    main: '.companies-wrapper',
+    wrap: '.companies-hor',
+    slidesToShow: 4,
+    infinity: true,
+    responsive: [{
+      breakpoint: 1024, 
+      slidesToShow: 3,
+    },
+    {
+      breakpoint: 768, 
+      slidesToShow: 2,
+    },
+    {
+      breakpoint: 576, 
+      slidesToShow: 1,
+    },
+    ]
+  });
+  carousel.init();
