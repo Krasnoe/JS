cc3948b2fb075c99e37328a5594632d8f0cbbef2
Krasnoe <psp.tema@mail.ru>
2021-08-16 20:33:00 +0300

    fix(bundle.js): comment const modalMenu

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -1,5 +1,6 @@
 const toggleMenu = () => {
-
+  // const modalMenu = document.querySelector('.modal-callback'); //работает
+  // console.log(modalMenu); 
   const handlerMenu = () => {
     document.querySelector('.modal-callback').classList.toggle('active-menu');
     document.querySelector('.modal-overlay').classList.toggle('active-menu');
