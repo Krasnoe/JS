8ce64567835f9ac5b4bde01c71dd7e28c350f2e6
Krasnoe <psp.tema@mail.ru>
2021-08-17 15:32:06 +0300

    feat(bundle.js): added f scrolls

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@
 
 import toggleMenu from './modules/toggleMenu';
 import slider from './modules/slider';
+import scrolls from './modules/scrolls';
 
 
 
 toggleMenu();
 
 slider(3000, true);
+
+scrolls();
