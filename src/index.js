d14a59c38bf881f752c531fcf4cfa92f9c820300
Krasnoe <psp.tema@mail.ru>
2021-08-17 19:31:25 +0300

    feat(bundle.js): added f SliderCarousel and .before to f toggleMenu

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import toggleMenu from './modules/toggleMenu';
 import slider from './modules/slider';
 import scrolls from './modules/scrolls';
-
+import SliderCarousel from './modules/SliderCarousel';
 
 
 toggleMenu();
@@ -11,3 +11,26 @@ toggleMenu();
 slider(3000, true);
 
 scrolls();
+
+const carousel = new SliderCarousel({
+  main: '.services-elements',
+  wrap: '.services-carousel',
+  next: '.arrow-right',
+  prev: '.arrow-left',
+  slidesToShow: 3,
+  infinity: true,
+  responsive: [{
+    breakpoint: 1024, 
+    slidesToShow: 3,
+  },
+  {
+    breakpoint: 768, 
+    slidesToShow: 2,
+  },
+  {
+    breakpoint: 576, 
+    slidesToShow: 1,
+  },
+  ]
+});
+carousel.init();
--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -1,6 +1,4 @@
 const toggleMenu = () => {
-  // const modalMenu = document.querySelector('.modal-callback'); //работает
-  // console.log(modalMenu); 
   const handlerMenu = () => {
     document.querySelector('.modal-callback').classList.toggle('active-menu');
     document.querySelector('.modal-overlay').classList.toggle('active-menu');
@@ -19,10 +17,48 @@ const toggleMenu = () => {
 
   document.addEventListener('click', (event) =>{
     let currentClick = event.target;
-    if(currentClick.matches('.callback-btn, .modal-overlay, .button-services') || currentClick.closest('.modal-close')){
+    if(currentClick.matches('.callback-btn, .modal-overlay, .button-services, .before') || currentClick.closest('.modal-close')){
       handlerMenu();
     } 
     
   });
+
+  const wrapper = document.querySelectorAll('.img-wrapper');
+
+  for(let i = 0; i < wrapper.length; i++){
+    if(wrapper[i].closest('.col-sm-6')) {
+      const before = document.createElement('div');
+      before.className = 'before';
+      before.textContent = 'Оформить заявку';
+      wrapper[i].appendChild(before);
+    }
+  }
+
+  const style = document.createElement('style');
+  document.head.appendChild(style);
+  style.textContent = `
+    .before {
+      display: block !important;
+      position: absolute !important;
+      width: 200px !important;
+      height: 48px !important;
+      top: 50% !important;
+      left: 50% !important;
+      margin-left: -100px !important;
+      -webkit-transition: all 200ms ease-out !important;
+      -moz-transition: all 200ms ease-out !important;
+      -o-transition: all 200ms ease-out !important;
+      transition: all 200ms ease-out !important;
+      margin-top: -24px !important;
+      z-index: 10 !important;
+      opacity: 0 !important;
+      color: #fff !important;
+      padding: 10px 0 !important;
+      text-align: center !important;
+      text-transform: uppercase !important;
+      border: 2px solid #fff !important;
+      cursor: pointer;
+    }
+  `;
 };
 export default toggleMenu;
